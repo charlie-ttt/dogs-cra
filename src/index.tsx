d380ff426510bc22c7ff146c46823b9caaf0da1f
Charlie <achanont@gmail.com>
2023-07-19 23:41:17 +0700

    add router and pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
+import SignUp from "./pages/SignUp";
 import theme from "./theme";
 
 const rootElement = document.getElementById("root");
@@ -12,6 +14,11 @@ root.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   </ThemeProvider>
 );
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <div>hello signup</div>;
+}
+
+export default Page;
