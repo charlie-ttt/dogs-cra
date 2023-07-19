3f900d634a486be2bb011942e30b89d1a7991f2a
Charlie <achanont@gmail.com>
2023-07-20 00:41:08 +0700

    add navigation bar

---
+++ src/components/layout/NavigationBar.tsx
@@ -0,0 +1,107 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuItem from "@mui/material/MenuItem";
+import signOut from "../../firebase/auth/signout";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const drawerWidth = 240;
+
+const routes = [
+  {
+    id: 1,
+    label: "All Dogs",
+    path: "/",
+  },
+  {
+    id: 2,
+    label: "View Feed",
+    path: "/feed",
+  },
+  {
+    id: 3,
+    label: "Sign Up",
+    path: "/signup",
+  },
+  {
+    id: 2,
+    label: "Sign In",
+    path: "/signin",
+  },
+];
+
+const isActiveRoute = (routeName: string, currentRoute: string) => {
+  return routeName === currentRoute;
+};
+
+export default function NavigationBar(props: React.PropsWithChildren) {
+  let location = useLocation();
+  const navigate = useNavigate();
+
+  console.log(location);
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Dogs App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <List>
+          {routes.map(({ label, path }, index) => (
+            <MenuItem selected={isActiveRoute(path, location?.pathname)}>
+              <ListItem key={label} disablePadding>
+                <ListItemButton onClick={() => navigate(path)}>
+                  <ListItemIcon></ListItemIcon>
+                  <ListItemText primary={label} />
+                </ListItemButton>
+              </ListItem>
+            </MenuItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          <ListItemButton sx={{ pl: 4 }} onClick={() => signOut()}>
+            <ListItemText primary="Log out" />
+          </ListItemButton>
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3, mt: 10 }}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  );
+}
